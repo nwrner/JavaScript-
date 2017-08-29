36ceff18dc8f1ce8d1267295ef786551f154e8b9
nwrner <nwrner516@gmail.com>
2017-08-29 12:01:06 -0500

    The Basics

---
+++ The Basics.js
@@ -0,0 +1,11 @@
+//Celsius to F converter
+var temp=prompt("Enter your C to F: "); 
+var celciusConv=temp*1.8+32;
+console.log(celciusConv)
+alert("Your temperature in C is: " + temp + " and your temperature in F is: " + celciusConv)
+
+//Alternative to lengthy if/else statements
+var isGoing = false;
+var color = isGoing ? "yellow" : "red";
+console.log(color);
+
